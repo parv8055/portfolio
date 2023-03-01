b4377ae958efd6d3f50e9d242b513927721d91f5
parv8055 <choudharyparv4@gmail.com>
2023-03-01 11:28:45 +0530

    ok

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -25,12 +25,11 @@ const Portfolio = () => {
                     </div>
                     <h3>seenUnseen(ecommerce project)</h3>
                     <div className="portfolio__item-cta">
-                        <a href="https://github.com/parv8055/memoriesss" className='btn' target='_blank'>Github</a>
+                        <a href="" className='btn' target='_blank'>Github</a>
                         <a href="https://seen-unseen.vercel.app/" className='btn btn-primary'
                             target='_blank'>Live Demo</a>
                     </div>
                 </article>
-
                 <article className='portfolio__item'>
                     <div className='portfolio__item-image'>
                         <img src={IMG9} alt="" />
@@ -38,23 +37,10 @@ const Portfolio = () => {
                     <h3>Amazon(nextjs)</h3>
                     <div className="portfolio__item-cta">
                         <a href="https://github.com/parv8055/amazon" className='btn' target='_blank'>Github</a>
-                        <a href="https://amaz.vercel.app/" className='btn btn-primary'
+                        <a href="https://amazon-six-red.vercel.app/" className='btn btn-primary'
                             target='_blank'>Live Demo</a>
                     </div>
                 </article>
-
-                <article className='portfolio__item'>
-                    <div className='portfolio__item-image'>
-                        <img src={IMG10} alt="" />
-                    </div>
-                    <h3>Dashboard(React)</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://github.com/parv8055/Dashboard" className='btn' target='_blank'>Github</a>
-                        <a href="https://dashbo.vercel.app/" className='btn btn-primary'
-                            target='_blank'>Live Demo</a>
-                    </div>
-                </article>
-
                 <article className='portfolio__item'>
                     <div className='portfolio__item-image'>
                         <img src={IMG1} alt="" />
@@ -62,7 +48,7 @@ const Portfolio = () => {
                     <h3>imdb(React)</h3>
                     <div className="portfolio__item-cta">
                         <a href="https://github.com/parv8055/Imdb" className='btn' target='_blank'>Github</a>
-                        <a href="https://imdb-n.vercel.app/" className='btn btn-primary'
+                        <a href="https://imdb-lac.vercel.app/" className='btn btn-primary'
                             target='_blank'>Live Demo</a>
                     </div>
                 </article>
@@ -73,7 +59,7 @@ const Portfolio = () => {
                     <h3>lyriks(React)</h3>
                     <div className="portfolio__item-cta">
                         <a href="https://github.com/parv8055/Lyriks" className='btn' target='_blank'>Github</a>
-                        <a href="https://lyrikss.vercel.app/" className='btn btn-primary'
+                        <a href="https://lyriks-chi.vercel.app/" className='btn btn-primary'
                             target='_blank'>Live Demo</a>
                     </div>
                 </article>
@@ -84,51 +70,7 @@ const Portfolio = () => {
                     <h3>Tube(React)</h3>
                     <div className="portfolio__item-cta">
                         <a href="https://github.com/parv8055/Tube" className='btn' target='_blank'>Github</a>
-                        <a href="https://uotube.vercel.app/" className='btn btn-primary'
-                            target='_blank'>Live Demo</a>
-                    </div>
-                </article>
-                <article className='portfolio__item'>
-                    <div className='portfolio__item-image'>
-                        <img src={IMG7} alt="" />
-                    </div>
-                    <h3>Cart(React)</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="" className='btn' target='_blank'>Github</a>
-                        <a href="https://caart.vercel.app/" className='btn btn-primary'
-                            target='_blank'>Live Demo</a>
-                    </div>
-                </article>
-                <article className='portfolio__item'>
-                    <div className='portfolio__item-image'>
-                        <img src={IMG3} alt="" />
-                    </div>
-                    <h3>Netmeds(HTML)</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://github.com/parv8055/Netmedsclone" className='btn' target='_blank'>Github</a>
-                        <a href="https://netmedss.netlify.app/" className='btn btn-primary'
-                            target='_blank'>Live Demo</a>
-                    </div>
-                </article>
-                <article className='portfolio__item'>
-                    <div className='portfolio__item-image'>
-                        <img src={IMG4} alt="" />
-                    </div>
-                    <h3>Spark America(HTML)</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://github.com/parv8055/sparkamerica-clone" className='btn' target='_blank'>Github</a>
-                        <a href="https://sparkamericaa.netlify.app/" className='btn btn-primary'
-                            target='_blank'>Live Demo</a>
-                    </div>
-                </article>
-                <article className='portfolio__item'>
-                    <div className='portfolio__item-image'>
-                        <img src={IMG5} alt="" />
-                    </div>
-                    <h3>Tindog(HTML)</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://github.com/parv8055/tindog" className='btn' target='_blank'>Github</a>
-                        <a href="https://tindogggg.netlify.app/" className='btn btn-primary'
+                        <a href="https://tube-xi.vercel.app/" className='btn btn-primary'
                             target='_blank'>Live Demo</a>
                     </div>
                 </article>
