0dd0e5a7a15954d01f5d59ddbe043c24de7c0a3d
parv8055 <choudharyparv4@gmail.com>
2023-03-05 19:59:27 +0530

    okay

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,14 +2,8 @@ import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/imdb.png'
 import IMG2 from '../../assets/lyriks.png'
-import IMG3 from '../../assets/netmeds.png'
-import IMG4 from '../../assets/spark.png'
-import IMG5 from '../../assets/tindog.png'
 import IMG6 from '../../assets/tube.png'
-import IMG7 from '../../assets/cart.png'
-import IMG8 from '../../assets/memories.png'
 import IMG9 from '../../assets/Amazon.png'
-import IMG10 from '../../assets/dashBoard.png'
 import IMG11 from '../../assets/seenunseen.png'
 
 const Portfolio = () => {
@@ -17,7 +11,6 @@ const Portfolio = () => {
         <section id='portfolio'>
             <h5>My Recent Work</h5>
             <h2>Portfolio</h2>
-
             <div className='container portfolio__container'>
                 <article className='portfolio__item'>
                     <div className='portfolio__item-image'>
@@ -45,7 +38,7 @@ const Portfolio = () => {
                     <div className='portfolio__item-image'>
                         <img src={IMG1} alt="" />
                     </div>
-                    <h3>imdb(React)</h3>
+                    <h3>imdb(Reactjs)</h3>
                     <div className="portfolio__item-cta">
                         <a href="https://github.com/parv8055/Imdb" className='btn' target='_blank'>Github</a>
                         <a href="https://imdb-lac.vercel.app/" className='btn btn-primary'
@@ -56,7 +49,7 @@ const Portfolio = () => {
                     <div className='portfolio__item-image'>
                         <img src={IMG2} alt="" />
                     </div>
-                    <h3>lyriks(React)</h3>
+                    <h3>lyriks(Reactjs)</h3>
                     <div className="portfolio__item-cta">
                         <a href="https://github.com/parv8055/Lyriks" className='btn' target='_blank'>Github</a>
                         <a href="https://lyriks-chi.vercel.app/" className='btn btn-primary'
@@ -67,7 +60,7 @@ const Portfolio = () => {
                     <div className='portfolio__item-image'>
                         <img src={IMG6} alt="" />
                     </div>
-                    <h3>Tube(React)</h3>
+                    <h3>Tube(Reactjs)</h3>
                     <div className="portfolio__item-cta">
                         <a href="https://github.com/parv8055/Tube" className='btn' target='_blank'>Github</a>
                         <a href="https://tube-xi.vercel.app/" className='btn btn-primary'
